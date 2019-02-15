1c74db9459c52af8bf5cfca6c6997e0e484933e6
Aditya Mohan <adityamohan885@gmail.com>
2019-02-15 12:04:27 +0530

    optimized O(n)

--- array_prob8.cpp
+++ array_prob8.cpp
@@ -1,7 +1,7 @@
 //https://practice.geeksforgeeks.org/problems/leaders-in-an-array/0
 
 #include <iostream>
-#include<stdio.h>
+#include <stdio.h>
 using namespace std;
 
 int main() {
@@ -13,27 +13,36 @@ int main() {
 	    int n;
 	    scanf("%d",&n);
 	    int arr[n];
+	    int len[n]={0};
+	    bool flag[n]={0};
+	    flag[n-1]=true;
+	    int last_j=n-1;
 	    for(int j=0;j<n;j++)
 	    {
 	        scanf("%d",&arr[j]);
 	    }
-        for(int j=0;j<n;j++)
-        {
-            int flag=0;
-            for(int k=j+1;k<n;k++)
-            {
-                if(arr[j]-arr[k]<0)
-                {
-                    flag++;
-                    break;
-                }
-            }
-            if(flag==0)
-            {
-                printf("%d",arr[j]);
-                printf(" ");
-            }
-        }
+	    for(int j=n-2;j>=0;j--)
+	    {
+	        if(arr[j]>=arr[last_j])
+	        {
+	            len[j]++;
+	            flag[j]=true;
+	            last_j=j;
+	        }
+	    }
+	    for(int j=0;j<n;j++)
+	    {
+	        if(flag[j]==true)
+	        {
+	            if(len[j]>1)
+	            {
+	                for(int k=0;k<len[j];k++)
+	                    printf("%d ",arr[j]);
+	            }
+	            else
+	                printf("%d ",arr[j]);
+	        }
+	    }
         printf("\n");
 	}
 	return 0;
